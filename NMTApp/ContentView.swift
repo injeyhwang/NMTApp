5cbdcb698e604f3c1a156ce1e194a27f8c3eb36a
Michael Hwang <michaelinjeyhwang@gmail.com>
2025-04-08 13:46:40 -0400

    initial Xcode project commit

---
+++ NMTApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NMTApp
+//
+//  Created by In Jey Hwang on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NMTApp/NMTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMTAppApp.swift
+//  NMTApp
+//
+//  Created by In Jey Hwang on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NMTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
